424ef3a9ee043840355fe0cb572600d4c42a8558
viccky834 <vjvadharia@gmail.com>
2015-12-14 11:10:21 +0530

    Create Inv.java

---
+++ Inv.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2015 Ericsson AB. All rights reserved
+ *
+ * DX ECIM Tool-chain generated this file on 2015-02-03 10:19:28 using: com.ericsson.ecim.mmas.cmpojo (2.13.0.201412190120)
+ */
+package com.ericsson.bss.rm.coba.core.cm.ecim.pojo;
+
+// The following imports are required for all CM POJOs
+import com.ericsson.caf.cm.ConfigurableBean;
+import com.ericsson.caf.cm.modeling.Configurable;
+import com.ericsson.caf.cm.modeling.ConfigurableField;
+
+/**
+ * .
+ *
+ * @version 1
+ * @generated
+ */
+@Configurable
+public class Coba extends ConfigurableBean
+{
+
+    /**
+     * .
+     */
+    @ConfigurableField
+    public String cobaId;
+
+    /**
+     * .
+     */
+    @ConfigurableField
+    public CobaCilNotification cobaCilNotificationId;
+
+    private static final long serialVersionUID = 1L;
+
+    public Coba()
+    {
+    }
+}
